58bf712804445a5c1ded221b0bed2600df1e3e52
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2019-08-04 16:56:24 -0400

    Apply new format, refactor, write out overview table

--- compile_lists.py
+++ compile_lists.py
@@ -5,6 +5,10 @@
 One json dump is sorted by year and then group, the other one by group first
 and then by year.
 """
+# TODO
+# How to deal with joint workshops? Difficulty comes from "seeing" their end
+# Should we add chairs
+# Missing Adjourn: AGING 2000, EFRW 2000
 
 import os
 from collections import defaultdict
@@ -13,64 +17,128 @@ from json import dumps
 from os.path import basename, join, splitext
 
 import pandas as pd
+from tabulate import tabulate
 
 SOURCE = "./source/"
 TARGET = "./output/"
 
+_end_categories = ("BREAKFAST", "BREAK", "LUNCH", "ADJOURN")
 
-def main():
-    # Read list of files
-    files = list()
-    for root, subdirs, filenames in os.walk(SOURCE):
+
+def correct_time(title, group, year, df, start=None, end=None):
+    """Correct the start or end time for a presentation."""
+    mask = ((df["title"] == title) & (df["group"] == group) &
+            (df["year"] == year))
+    if start:
+        df.loc[mask, "start"] = start
+    if end:
+        df.loc[mask, "end"] = end
+
+
+def list_files():
+    """List files in nested subfolders."""
+    for root, subdirs, filenames in sorted(os.walk(SOURCE)):
         for filename in filenames:
             if not filename.endswith('dat'):
                 continue
-            files.append(join(root, filename))
+            yield join(root, filename)
 
+
+def main():
     # Compile each file separately
     by_year = defaultdict(lambda: defaultdict(list))
     by_group = defaultdict(lambda: defaultdict(list))
-    df = pd.DataFrame()
-    for file in files:
+    by_title = pd.DataFrame()
+    for file in list_files():
         group = splitext(basename(file))[0]
         year = int(file.split("/")[-2])
+        if year >= 2012:  # hasn't been parsed yet
+            continue
         with open(file, 'r') as inf:
             lines = inf.readlines()
-        for line in lines:
+        meta = {'group': group, 'year': year}  # General information
+        entry = False  # Help filtering header information
+        joint = None  # To possibly add other authors
+        d = {}
+        add_start = []
+        add_end = []
+        for num, line in enumerate(lines):
+            last_line = num == len(lines)-1
+            end_of_entry = "end" in d
             tokens = line.strip().split(': ', 1)
             cat = tokens[0]
-            if cat == "TITLE":
-                title = tokens[1].strip()
-                d = {'title': title, 'authors': [], "discussants": []}
-            elif cat == "AUTH":
-                auth = tokens[1].split(",")[0].replace(".", "")
-                d['authors'].append(auth)
-            elif cat == "DIS":
-                discussants = tokens[1].replace(".", "").split(";")
-                try:
-                    discussants.remove("-")
-                except:
-                    pass
-                for dis in discussants:
-                    dis = dis.split(",")[0].strip()
-                    d['discussants'].append(dis)
-            elif cat == "":
-                # by_group[group][year].append(d)
-                # by_year[year][group].append(d)
-                d['authors'] = "; ".join(d['authors'])
-                d['discussants'] = "; ".join(d['discussants'])
-                d.update({'group': group, 'year': year})
-                df = df.append(d, ignore_index=True)
+            if cat in ("DATE", "VENUE", "ORGANIZER"):
+                meta[cat.lower()] = tokens[1].strip()
+            elif cat == "TITLE":
+                if entry:  # Get end in next line to finish current entry
+                    next_line = lines[num+1].strip()
+                    if next_line.split(': ', 1)[0] == "TIME":
+                        end = next_line.split(": ", 1)[-1]
+                        d["end"] = end
+                        for t in add_end:
+                            correct_time(t, group, year, by_title, end=end)
+                        add_end = []
+                    else:
+                        add_end.append(d["title"])
+                    # Finalize
+                    if joint:
+                        d["joint"] = joint
+                    by_group[group][year].append(d)
+                    by_year[year][group].append(d)
+                    d.update(meta)
+                    by_title = by_title.append(d, ignore_index=True)
+                    if not "start" in d:
+                        add_start.append(d["title"])
+                d = {"title": tokens[1]}
+                entry = True
+            elif cat in ("AUTHOR", "DISCUSSANT", "LINK"):
+                d[cat.lower()] = tokens[1]
+            elif cat == "JOINT":
+                if entry:
+                    d["author"] += " " + tokens[1]
+                else:
+                    joint = tokens[1]
+            elif cat == "TIME":
+                d["start"] = tokens[1]
+                # For presentations w/o start, add previous start time
+                for t in add_start:
+                    correct_time(t, group, year, by_title, start=start)
+                add_start = []
+                start = tokens[1]
+            elif cat in _end_categories:
+                d["end"] = tokens[1]
+                # For presentations w/o end, add this end time
+                for t in add_end:
+                    correct_time(t, group, year, by_title, end=tokens[1])
+                add_end = []
+            elif (last_line and not end_of_entry) or (cat == "" and entry and end_of_entry):
+                # Finalize
+                if joint:
+                    d["joint"] = joint
+                by_group[group][year].append(d)
+                by_year[year][group].append(d)
+                d.update(meta)
+                by_title = by_title.append(d, ignore_index=True)
+                if not "start" in d:
+                    add_start.append(d["title"])
+                # For presentations w/o start, add previous start time
+                for t in add_start:
+                    correct_time(t, group, year, by_title, start=start)
+                add_start = []
+                entry = False
 
     # Write out
     with open(TARGET + 'by_year.json', 'w') as ouf:
         ouf.write(dumps(by_year, indent=2, sort_keys=True))
     with open(TARGET + 'by_group.json', 'w') as ouf:
-        dumps(by_group, indent=2, sort_keys=True)
-    df['year'] = df['year'].astype(int)
-    df.to_csv(TARGET + "by_title.csv")
-    overview = pd.crosstab(df['group'], df['year'])
-    # Find way to write as markdown
+        ouf.write(dumps(by_group, indent=2, sort_keys=True))
+    by_title["year"] = by_title["year"].astype(int)
+    order = ['group', 'year', 'date', 'venue', 'organizer', 'title', 'author',
+             'discussant', 'start', 'end', 'link']
+    by_title[order].to_csv(TARGET + "by_title.csv", index=False)
+    overview = pd.crosstab(by_title['group'], by_title['year'])
+    with open("overview.md", "w") as ouf:
+        ouf.write(tabulate(overview, tablefmt="pipe", headers="keys"))
 
 
 if __name__ == '__main__':
